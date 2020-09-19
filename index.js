5bbdecf5e717e1db720357055b1d670be7ff3192
Devinhodge <d.hodge8891@yahoo.com>
2020-09-19 12:34:05 -0500

    created routers

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 const port = process.env.PORT || 4000
+
+
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => res.send('default route'))
 
